a6270ffdebe8bfe429d867319a45b150a74db480
Torilen <saeran.ion@gmail.com>
2020-04-14 23:38:46 +0200

    add logger info

--- train.py
+++ train.py
@@ -98,6 +98,7 @@ def main(config):
     bert_base_model.config.num_labels = num_labels
 
     if args.dataset in {'camera', 'car', 'phone', 'notebook', 'ter_data'}:
+	logger.info("Convert polarity")
         convert_polarity(train_examples)
         convert_polarity(eval_examples)
         model = LCF_ATEPC(bert_base_model, args=args)
