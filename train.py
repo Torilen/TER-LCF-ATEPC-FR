e306a7c4adc9b7651e5c3267916a75d63d9c8fc0
Aniss <saeran.ion@gmail.com>
2020-05-09 16:55:12 +0200

    canephore add and experiment

--- train.py
+++ train.py
@@ -63,6 +63,7 @@ def main(config):
         'twitter': "atepc_datasets/twitter",
         'mixed': "atepc_datasets/mixed",
         'ter_data': "atepc_datasets/ter_data",
+        'canephore': "atepc_datasets/canephore",
     }
     pretrained_bert_models = {
         'camera': "bert-base-chinese",
@@ -74,6 +75,7 @@ def main(config):
         'twitter': "bert-base-uncased",
         'mixed': "bert-base-multilingual-uncased",
         'ter_data': "bert-base-multilingual-cased",
+        'canephore': "bert-base-multilingual-cased",
     }
 
     args.bert_model = pretrained_bert_models[args.dataset]
@@ -97,7 +99,7 @@ def main(config):
     bert_base_model = BertModel.from_pretrained(args.bert_model)
     bert_base_model.config.num_labels = num_labels
 
-    if args.dataset in {'camera', 'car', 'phone', 'notebook', 'ter_data'}:
+    if args.dataset in {'camera', 'car', 'phone', 'notebook', 'ter_data', 'canephore'}:
         logger.info("Convert polarity")
         convert_polarity(train_examples)
         convert_polarity(eval_examples)
@@ -190,7 +192,7 @@ def main(config):
                             temp_2.append(label_map.get(ate_logits[i][j], 'O'))
         if eval_APC:
             test_acc = n_test_correct / n_test_total
-            if args.dataset in {'camera', 'car', 'phone', 'notebook', 'ter_data'}:
+            if args.dataset in {'camera', 'car', 'phone', 'notebook', 'ter_data', 'canephore'}:
                 test_f1 = f1_score(torch.argmax(test_apc_logits_all, -1).cpu(), test_polarities_all.cpu(),
                                    labels=[0, 1], average='macro')
             else:
@@ -340,7 +342,7 @@ def parse_experiments(path):
 if __name__ == "__main__":
 
     experiments = argparse.ArgumentParser()
-    experiments.add_argument('--config_path', default='french_experiments.json', type=str,help='Path of experiments config file')
+    experiments.add_argument('--config_path', default='canephore.json', type=str,help='Path of experiments config file')
     experiments = experiments.parse_args()
 
     from utils.Pytorch_GPUManager import GPUManager
